4ee67eb44dabac481465a79a31a8d4117a1ea811
Luis Salazar O <luiyi_salazar@hotmail.com>
2024-01-08 18:04:48 +0000

    Add endpoints for User, People and Planets

--- src/app.py
+++ src/app.py
@@ -8,8 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User, People, Planet, Favorites
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -36,6 +35,8 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+
+# Endpoint to get all users
 @app.route('/user', methods=['GET'])
 def handle_hello():
 
@@ -44,6 +45,40 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+# Endpoint to get all people
+@app.route('/people', methods=['GET'])
+def get_people():
+    people = People.query.all()
+    serialized_people = [person.serialize() for person in people]
+    return jsonify(serialized_people), 200
+
+# Endpoint to get a sinle person by ID
+@app.route('/people/<int:people_id>', methods=['GET'])
+def get_person(people_id):
+    person = People.query.get(people_id)
+    if person:
+        serialized_person = person.serialize()
+        return jsonify(serialized_person), 200
+    else:
+        return jsonify({"error": "Person not found"}), 404
+    
+# Endpoint to get all planets
+@app.route('/planets', methods=['GET'])
+def get_planets():
+    planets = Planet.query.all()
+    serialized_planets = [planet.serialize() for planet in planets]
+    return jsonify(serialized_planets), 200
+
+# Endpoint to get a single planet by ID
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_planet(planet_id):
+    planet = Planet.query.get(planet_id)
+    if planet:
+        serialized_planet = planet.serialize()
+        return jsonify(serialized_planet), 200
+    else:
+        return jsonify({"error": "Planet not found"}), 404
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
